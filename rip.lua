1f89de72a8144399f497f6115bc73bd639a2b286
idk <aidenhancock1406@gmail.com>
2025-09-17 22:20:35 -0400

    Update rip.lua

--- rip.lua
+++ rip.lua
@@ -4,6 +4,6 @@ local keyScript = loadstring(game:HttpGet("https://raw.githubusercontent.com/adm
 -- Validate the key
 keyScript(function()
     -- Load and execute the hm.lua script after key validation
-    local mainScript = loadstring(game:HttpGet("https://raw.githubusercontent.com/adminabuser-creator/terst/main/mainn.lua"))()
+    local mainScript = loadstring(game:HttpGet("https://raw.githubusercontent.com/adminabuser/terst/refs/heads/main/mainn.lua"))()
     mainScript()
 end)
