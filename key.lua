4f518e96d8d61cf1e5c015fe4e22997c4e456ff6
aidenhancock1406-creator <aidenhancock1406@gmail.com>
2025-09-17 20:32:55 -0400

    Update key.lua

--- key.lua
+++ key.lua
@@ -1,1 +1,56 @@
+-- Define the key required to access the UI
+local requiredKey = "NFL123"
 
+-- Load the Orion library
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/aidenhancock1406-creator/terst/refs/heads/main/source.lua')))()
+
+-- Create the UI window
+local Window = OrionLib:MakeWindow({Name = "SwebHub - Key System", HidePremium = false, SaveConfig = false})
+
+-- Function to prompt the user for the key
+local function requestKey(onSuccess)
+    local isKeyValid = false
+
+    -- Create a tab for the key input
+    local Tab = Window:MakeTab({
+        Name = "Key Input",
+        Icon = "rbxassetid://4483345998",
+        PremiumOnly = false
+    })
+
+    -- Add a textbox for the user to input the key
+    Tab:AddTextbox({
+        Name = "Enter Key",
+        Default = "",
+        TextDisappear = true,
+        Callback = function(userKey)
+            if userKey == requiredKey then
+                isKeyValid = true
+                OrionLib:MakeNotification({
+                    Name = "Success",
+                    Content = "Key is valid! Access granted.",
+                    Image = "rbxassetid://4483345998",
+                    Time = 5
+                })
+                OrionLib:Destroy()
+                if onSuccess then
+                    onSuccess()
+                end
+            else
+                OrionLib:MakeNotification({
+                    Name = "Error",
+                    Content = "Invalid Key. Please try again.",
+                    Image = "rbxassetid://4483345998",
+                    Time = 5
+                })
+            end
+        end
+    })
+
+    -- Initialize the UI and wait until the key is valid
+    OrionLib:Init()
+    repeat wait() until isKeyValid
+end
+
+-- Execute the key request function
+return requestKey
