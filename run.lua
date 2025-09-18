41002ad1c85b7f6900a463e5529a305d9d45a6f0
aidenhancock1406-creator <aidenhancock1406@gmail.com>
2025-09-17 21:13:53 -0400

    Update run.lua

--- run.lua
+++ run.lua
@@ -4,6 +4,6 @@ local keyScript = loadstring(game:HttpGet("https://raw.githubusercontent.com/aid
 -- Validate the key
 keyScript(function()
     -- Load and execute the hm.lua script after key validation
-    local mainScript = loadstring(game:HttpGet("https://raw.githubusercontent.com/aidenhancock1406-creator/terst/main/slime2.lua"))()
+    local mainScript = loadstring(game:HttpGet("https://raw.githubusercontent.com/aidenhancock1406-creator/terst/main/slime3.lua"))()
     mainScript()
 end)
